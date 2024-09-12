d34d1c65922a27393ea81022fcb21b410245afe6
djallal rahmoune <djallalrahmoune@gmail.com>
2024-09-12 20:29:52 +0200

    extractOneProductToCsv

--- oneProductOnePage.py
+++ oneProductOnePage.py
@@ -3,7 +3,6 @@ import csv
 from bs4 import BeautifulSoup #try to install it if it dosn't done yet
 
 url = "http://books.toscrape.com/catalogue/the-secret-of-dreadwillow-carse_944/index.html" #the choosen book  
-
 res = requests.get(url)
 
 soup = BeautifulSoup(res.text, 'html.parser')
@@ -12,22 +11,18 @@ desc = soup.find("p", class_ = "")#delect the descrioption of the product
 ths = soup.find_all('th')         #find all description of product's elmnts
 tds = soup.find_all('td')         #find all contents of prodcut's elmnts description
 
-#find elmnts of product's description
+#find titles of product's description
 col = []
 for th in ths:
-    #print(th.text)
-    tl = th.text
-    col.append(tl)
+    col.append(th.text)
+col.append('desc')
 
-#constitute a table of the product's description
+#find values of the product's description
 table = []
-i=0
+
 for td in tds:
-    #print(th.text)
-    ct = td.text
-    el = [col[i],ct]
-    table.append(el)
-    i+=1
+    table.append(td.text)
+table.append(desc.text)
 
 
 
@@ -36,9 +31,9 @@ for td in tds:
 with open('oneProductExtracted.csv', mode="w", newline="") as csvfile:
 
     writer = csv.writer(csvfile)
-    writer.writerow(['desc\t'+desc.text])
-    for el in table:
-        writer.writerow([el[0]+'\t'+el[1]])
+    writer.writerow(col)
+    writer.writerow(table)
+
     csvfile.close()
     
 
