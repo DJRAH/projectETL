12519d61e669f75d29ac0ea26dd5d5f2b13327f5
djallal rahmoune <djallalrahmoune@gmail.com>
2024-09-13 20:28:39 +0200

    writing all products of a category to a CSVfile

--- oneCatAllProduct.py
+++ oneCatAllProduct.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 from bs4 import BeautifulSoup #try to install it if it dosn't done yet
+import oneProductOnePage
 
 url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html" #the choosen book  
 res = requests.get(url)
@@ -9,12 +10,18 @@ soup = BeautifulSoup(res.text, 'html.parser')
 
 desc = soup.find_all("img")#recover all tags with links
 
+#constitue all links
 links=[]
-
 for im in desc:
     al = str((im.parent)['href'])
-    link = 'http://books.toscrape.com/catalogue/category/books/' + al.removeprefix("../../../")
+    link = 'https://books.toscrape.com/catalogue/' + al.removeprefix("../../../")
     links.append(link)
 
+#write all product of the category to a file
+i=0
 for li in links:
-    print(li)
+    oneProductOnePage.write(li,i)
+    i=1
+
+
+    
--- oneProductOnePage.py
+++ oneProductOnePage.py
@@ -2,38 +2,41 @@ import requests
 import csv
 from bs4 import BeautifulSoup #try to install it if it dosn't done yet
 
-url = "http://books.toscrape.com/catalogue/the-secret-of-dreadwillow-carse_944/index.html" #the choosen book  
-res = requests.get(url)
+def write(link,titleWritten):
+    url = link #the choosen book  
+    print(url)
+    res = requests.get(url)
 
-soup = BeautifulSoup(res.text, 'html.parser')
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-desc = soup.find("p", class_ = "")#delect the descrioption of the product
-ths = soup.find_all('th')         #find all description of product's elmnts
-tds = soup.find_all('td')         #find all contents of prodcut's elmnts description
+    desc = soup.find("p", class_ = "")#delect the descrioption of the product
+    ths = soup.find_all('th')         #find all description of product's elmnts
+    tds = soup.find_all('td')         #find all contents of prodcut's elmnts description
 
-#find titles of product's description
-col = []
-for th in ths:
-    col.append(th.text)
-col.append('desc')
+    #find titles of product's description
+    col = []
+    for th in ths:
+        col.append(th.text)
+    col.append('desc')
 
-#find values of the product's description
-table = []
+    #find values of the product's description
+    table = []
 
-for td in tds:
-    table.append(td.text)
-table.append(desc.text)
+    for td in tds:
+        table.append(td.text)
+    table.append(desc.text)
 
 
 
-#writing in a file
+    #writing in a file
 
-with open('oneProductExtracted.csv', mode="w", newline="") as csvfile:
+    with open('oneProductExtracted.csv', mode="a", newline="") as csvfile:
 
-    writer = csv.writer(csvfile)
-    writer.writerow(col)
-    writer.writerow(table)
+        writer = csv.writer(csvfile)
+        if titleWritten==0:
+            writer.writerow(col)
+        writer.writerow(table)
 
-    csvfile.close()
-    
+        
+        
 
